d5cb25987e6c6400433c299041a11933893ac050
CésarL <107433989+Cesar0208@users.noreply.github.com>
2026-04-06 17:01:08 -0300

    Add files via upload

---
+++ UC9/React Native/Aula1/index.tsx
@@ -0,0 +1,10 @@
+function soma(a: number, b: number): number {
+    return a + b;
+}
+console.log(soma(5, 10));
+
+function calcularTotal(preco: number, quantidade: number): number {
+    return preco * quantidade;
+}
+
+console.log(calcularTotal(10, 8));
